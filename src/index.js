b36cb817a74f977d7c069b80e1c45cd68eb0870b
aayrton <hair.thon@gmail.com>
2016-12-16 15:24:57 +0100

    Remove material-ui package

--- src/index.js
+++ src/index.js
@@ -4,26 +4,17 @@ import {Router, browserHistory} from 'react-router';
 import {Provider} from 'react-redux';
 import {fetchPets} from './app/actions/petActions';
 import configStore from './app/store/configStore';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import routes from './routes';
 import './index.scss';
 
-// Needed for onTouchTap (marerial-ui)
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-
 const store = configStore();
 
-store.dispatch(fetchPets()).then(() => {
-});
+store.dispatch(fetchPets());
 
 render(
-  <MuiThemeProvider>
-    <Provider store={store}>
-      <Router history={browserHistory} routes={routes}/>
-    </Provider>
-  </MuiThemeProvider>,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>,
   document.getElementById('root')
 );
 
